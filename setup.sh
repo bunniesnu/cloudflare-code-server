8ffe9c8c4a894c78e4a1e08f2dce088e3ac10691
Bunniesnu <bunniesnu@gmail.com>
2026-01-31 07:19:07 +0000

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit 1
+mkdir -p ./cf-admin
+sudo chown 65532:65532 ./cf-admin
+source .env
+sed "s|code.example.com|$DOMAIN|g" config_orig.yml > config.yml
+docker run --rm -it -v ./cf-admin:/home/nonroot/.cloudflared cloudflare/cloudflared:latest tunnel login
+docker run --rm -it -v ./cf-admin:/home/nonroot/.cloudflared cloudflare/cloudflared:latest tunnel create code-server
+docker run --rm -it -v ./cf-admin:/home/nonroot/.cloudflared cloudflare/cloudflared:latest tunnel route dns code-server $DOMAIN
+sudo mv $(find ./cf-admin -type f -name "*.json") ./tunnel.json
+sudo rm -r ./cf-admin
+docker compose run --rm init-permissions
